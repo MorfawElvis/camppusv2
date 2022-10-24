7efd496440dd6a238af3ea4828507c65dfaf0ed4
MorfawElvis <elvismorfaw@gmail.com>
2022-10-24 04:49:36 +0100

    HomeController modified

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,30 +9,35 @@ use App\Models\Student;
 
 class HomeController extends Controller
 {
+    private $class_room, $enrollment;
     
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    public function test()
+    {
+
+    }
+
     public function index()
     {
+            
             $enrollment_boys = Student::where('gender', 'M')->count();
             $enrollment_girls = Student::where('gender', 'F')->count();
             
             $class_rooms = ClassRoom::withCount('students')->get()->toArray();
             $array_size = count($class_rooms);
 
-            // $class_room = [];
-            // $enrollment = [];
             for($i=0; $i < $array_size; $i++){
-                $class_room[] =  $class_rooms[$i]['class_name']; 
-                $enrollment[] =  $class_rooms[$i]['students_count']; 
+                $this->class_room[] =  $class_rooms[$i]['class_name']; 
+                $this->enrollment[] =  $class_rooms[$i]['students_count']; 
             }
             //Enrollment analyses chart
             $enrollment_chart = new EnrollmentStatistics;
-            $enrollment_chart->labels($class_room);
-            $enrollment_chart->dataset('Enrollment analyses by class', 'bar', $enrollment)->backgroundColor(['maroon', 'orange','navy','purple', 'blue',
+            $enrollment_chart->labels($this->class_room);
+            $enrollment_chart->dataset('Enrollment analyses by class', 'bar', $this->enrollment)->backgroundColor(['maroon', 'orange','navy','purple', 'blue',
             'maroon', 'orange','navy','purple', 'blue','maroon', 'orange','navy','purple', 'blue','maroon', 'orange','navy','purple', 'blue']);
             //Gender analyses chart
             $gender_chart = new GenderAnalyses;
